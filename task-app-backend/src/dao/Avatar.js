f8ca935dcdc1c2768d8ce24409dfe9645a37643a
Shubham Khurana <khuranashubham23@gmail.com>
2024-05-10 22:45:46 +0530

    Task Manager App
    Signed-off-by: Shubham Khurana <khuranashubham23@gmail.com>

---
+++ task-app-backend/src/dao/Avatar.js
@@ -0,0 +1,13 @@
+const { Table } = require("./Table");
+
+class Avatar extends Table {
+  static schema = {
+    id: { isPK: true },
+    avatar_name: {},
+    link: {},
+  };
+}
+
+module.exports = {
+  Avatar,
+};
